bfe8447165d776e982ecb4f2d9c68be8a2832c20
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-08-22 12:51:45 +0100

    Fixes #1 and #2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,15 @@
 // Scripts
 import Typography from 'typography'
 import judahTheme from 'typography-theme-judah'
+import debounce from 'lodash.debounce'
+import { adjustHeight } from './utils/functions'
 
 const typography = new Typography(judahTheme)
 
+const debouncedAdjustHeight = debounce(adjustHeight, 500)
+
 typography.injectStyles()
+
+debouncedAdjustHeight()
+
+window.addEventListener('resize', debouncedAdjustHeight)
