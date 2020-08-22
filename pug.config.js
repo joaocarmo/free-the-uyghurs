4e361d9f3aa4c563933092f436fc616c7cbb128b
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-08-22 23:58:52 +0100

    Updated the copyright notice

--- pug.config.js
+++ pug.config.js
@@ -8,9 +8,12 @@ module.exports = {
       aboutLink: 'About',
       contributeLink: 'Contribute',
       contactLink: 'Contact',
-      aboutText: 'I believe no one should be subjected to hostility and ill-treatment, especially because of their race, political or religious beliefs. As I am unable to help directly those being persecuted, but I can use the skills I have to raise awareness and hopefully reach enough people. Together we have a strong voice and can hold accountable those responsible for such hedious crimes.',
-      aboutTextCredits: 'The main illustration is the work of <a href="https://www.behance.net/taniamatoss" target="_blank" rel="noopener noreferrer">Tania Matos</a>. Some SVGs have been used from <a href="https://svgsilh.com" target="_blank" rel="noopener noreferrer">svgsilh.com</a>, released under Creative Commons CC0.',
+      aboutText:
+        'I believe no one should be subjected to hostility and ill-treatment, especially because of their race, political or religious beliefs. As I am unable to help directly those being persecuted, but I can use the skills I have to raise awareness and hopefully reach enough people. Together we have a strong voice and can hold accountable those responsible for such hedious crimes.',
+      aboutTextCredits:
+        'The main illustration is the work of <a href="https://www.behance.net/taniamatoss" target="_blank" rel="noopener noreferrer">Tania Matos</a>. Some SVGs have been used from <a href="https://svgsilh.com" target="_blank" rel="noopener noreferrer">svgsilh.com</a>, released under Creative Commons CC0.',
     },
-    copyrightNotice: 'Copyright © 2020 João Carmo, Tania Matos. All Rights Reserved.',
+    copyrightNotice:
+      'Copyright © 2020 João Carmo, Tania Matos. Some Rights Reserved.',
   },
 }
