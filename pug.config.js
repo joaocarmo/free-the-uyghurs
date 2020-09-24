4f7e9983fa7c562f4edf983f1e3e8d85e62365d3
Joao Carmo <17721880+joaocarmo@users.noreply.github.com>
2020-09-24 22:10:25 +0100

    Added an icon to indicate visitors they should rotate their device

--- pug.config.js
+++ pug.config.js
@@ -9,9 +9,9 @@ module.exports = {
       contributeLink: 'Contribute',
       contactLink: 'Contact',
       aboutText:
-        'I believe no one should be subjected to hostility and ill-treatment, especially because of their race, political or religious beliefs. As I am unable to help directly those being persecuted, but I can use the skills I have to raise awareness and hopefully reach enough people. Together we have a strong voice and can hold accountable those responsible for such hedious crimes.',
+        'I believe no one should be subjected to hostility and ill-treatment, especially because of their race, political or religious beliefs. I am unable to help directly those being persecuted, but I can use the skills I have to raise awareness and hopefully reach enough people. Together we have a strong voice and can hold accountable those responsible for such hedious crimes.',
       aboutTextCredits:
-        'The main illustration is the work of <a href="https://www.behance.net/taniamatoss" target="_blank" rel="noopener noreferrer">Tania Matos</a>. Some SVGs have been used from <a href="https://svgsilh.com" target="_blank" rel="noopener noreferrer">svgsilh.com</a>, released under Creative Commons CC0.',
+        'The main illustration is the work of <a href="https://www.behance.net/taniamatoss" target="_blank" rel="noopener noreferrer">Tania Matos</a>. Some SVGs have been used from <a href="https://svgsilh.com" target="_blank" rel="noopener noreferrer">svgsilh.com</a>, released under Creative Commons CC0. Some icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank" rel="noopener noreferrer">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel="noopener noreferrer">www.flaticon.com</a>.',
     },
     copyrightNotice:
       'Copyright © 2020 João Carmo, Tania Matos. Some Rights Reserved.',
